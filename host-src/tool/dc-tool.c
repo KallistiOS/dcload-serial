b40f0504f1425c5473f937cd307a26245c064256
Colton Pawielski <cepawiel@mtu.edu>
2023-05-20 12:05:01 -0500

    Add support for a return code in the exit() syscall
    The return code passed to exit() on the target will be
    transfered to the host and used as the exit code for the
    host application.

--- host-src/tool/dc-tool.c
+++ host-src/tool/dc-tool.c
@@ -1064,8 +1064,7 @@ void do_console(unsigned char *path, unsigned char *isofile)
 
         switch (command) {
             case 0:
-                finish_serial();
-                exit(0);
+                dc_exit();
                 break;
             case 1:
                 dc_fstat();
